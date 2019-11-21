2bfd8de2e623a4e279053bad98d4a25dfccdbeb4
= <=>
2019-11-21 12:25:49 +0700

    first commit

---
+++ precache-manifest.8aaa43e0ca1f9548fcf9778afc4c71ff.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bab08fd2b3a853897dbd",
+    "url": "/css/about.d443fafb.css"
+  },
+  {
+    "revision": "4d99ece3fadede65bc7e",
+    "url": "/css/chunk-vendors.b2e3e4db.css"
+  },
+  {
+    "revision": "6aec92244873e32140c7678ee76458a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bab08fd2b3a853897dbd",
+    "url": "/js/about.67552e4e.js"
+  },
+  {
+    "revision": "35b588fcf2271638878d",
+    "url": "/js/app.63a22f50.js"
+  },
+  {
+    "revision": "4d99ece3fadede65bc7e",
+    "url": "/js/chunk-vendors.2d5a338b.js"
+  },
+  {
+    "revision": "2efb2ab03b39683dbbeb7946eb89893a",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
